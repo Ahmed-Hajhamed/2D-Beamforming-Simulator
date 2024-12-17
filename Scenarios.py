0f758d2d6f8c37609457a454c1f753d9c1451c81
ah03ad63 <125690012+ah03ad63@users.noreply.github.com>
2024-12-17 14:03:52 +0200

    Scenarios

--- Scenarios.py
+++ Scenarios.py
@@ -22,4 +22,9 @@ def ultrasound(meshgrid_x, meshgrid_y, beam_profile_x, beam_profile_y):
     return transducer_array, receivers
 
 def five_G(meshgrid_x, meshgrid_y, beam_profile_x, beam_profile_y):
-    pass
+    sender_array = Array("sender_array", "Linear", 16, 1, 0, [0, 0], 
+                         meshgrid_x, meshgrid_y, beam_profile_x, beam_profile_y, 0.5, radius= None, arc_angle= None)
+    receivers = {}
+    receivers["Receiver_1"] = Receiver("Receiver_1", [-8, 8])
+    receivers["Receiver_2"] = Receiver("Receiver_2", [8, 8])
+    return sender_array, receivers
