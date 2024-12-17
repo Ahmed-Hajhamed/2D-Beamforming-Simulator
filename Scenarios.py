10791d7809eb21273ba54e40f2ff8f805c466b11
ah03ad63 <125690012+ah03ad63@users.noreply.github.com>
2024-12-17 19:52:35 +0200

    Scenarios Updates

--- Scenarios.py
+++ Scenarios.py
@@ -1,30 +1,37 @@
 from PhasedArray import Array
 from Receiver import Receiver
+arrays = {}
+receivers = {}
 
 def tumor_ablation(meshgrid_x, meshgrid_y, beam_profile_x, beam_profile_y):
-    buttom_array = Array("buttom_array", "Linear", 8, 1, 0, [0, 0], 
+    buttom_array = Array("Buttom Array", "Linear", 8, 1, 0, [0, 0], 
                          meshgrid_x, meshgrid_y, beam_profile_x, beam_profile_y, 0.5, radius= None, arc_angle= None)
-    top_array = Array("top_array", "Linear", 8, 1, 180, [0, 8], 
+    top_array = Array("Top Array", "Linear", 8, 1, 180, [0, 8], 
                          meshgrid_x, meshgrid_y, beam_profile_x, beam_profile_y, 0.5, radius= None, arc_angle= None)
-    left_array = Array("left_array", "Linear", 8, 1, 90, [-8, 4], 
+    left_array = Array("Left Array", "Linear", 8, 1, 90, [-8, 4], 
                          meshgrid_x, meshgrid_y, beam_profile_x, beam_profile_y, 0.5, radius= None, arc_angle= None)
-    right_array = Array("top_array", "Linear", 8, 1, -90, [8, 4], 
+    right_array = Array("Right Array", "Linear", 8, 1, -90, [8, 4], 
                          meshgrid_x, meshgrid_y, beam_profile_x, beam_profile_y, 0.5, radius= None, arc_angle= None)
     tumor_receiver = Receiver("Tumor", [0, 4])
-    return buttom_array, top_array, left_array, right_array, tumor_receiver
+    arrays["Buttom Array"] = buttom_array
+    arrays["Top Array"] = top_array
+    arrays["Left Array"] = left_array
+    arrays["Right Array"] = right_array
+    receivers["Tumor"] = tumor_receiver
+    return arrays, receivers
 
 def ultrasound(meshgrid_x, meshgrid_y, beam_profile_x, beam_profile_y):
     transducer_array = Array("Transducer", "Curved", 64, 1, 0, [0, 0], meshgrid_x, meshgrid_y, beam_profile_x, 
                              beam_profile_y, element_spacing=None, radius=1, arc_angle=120)
-    receivers = {}
+    arrays["Transducer"] = transducer_array
     for i in range(8):
         receivers[f"Receiver_{i}"] = Receiver(f"Receiver_{i}",  [(-20 + 5 * i), 8])
-    return transducer_array, receivers
+    return arrays, receivers
 
 def five_G(meshgrid_x, meshgrid_y, beam_profile_x, beam_profile_y):
-    sender_array = Array("sender_array", "Linear", 16, 1, 0, [0, 0], 
+    sender_array = Array("Sender Array", "Linear", 16, 1, 0, [0, 0], 
                          meshgrid_x, meshgrid_y, beam_profile_x, beam_profile_y, 0.5, radius= None, arc_angle= None)
-    receivers = {}
     receivers["Receiver_1"] = Receiver("Receiver_1", [-8, 8])
     receivers["Receiver_2"] = Receiver("Receiver_2", [8, 8])
-    return sender_array, receivers
+    arrays["Sender Array"] = sender_array
+    return arrays, receivers
